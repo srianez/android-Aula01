6cdefd5e9d920108dcfbd21ea8cde5bd65f9f084
logonrm <logonrm@fiap.com.br>
2017-10-30 22:26:32 -0200

    first commit

---
+++ app/src/main/java/br/com/fiap/placar/PlacarActivity.java
@@ -0,0 +1,13 @@
+package br.com.fiap.placar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlacarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placar);
+    }
+}
