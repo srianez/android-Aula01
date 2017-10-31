66a494b40097e8aa199312c69eeb807d56c0bf1a
logonrm <logonrm@fiap.com.br>
2017-10-30 22:53:29 -0200

    segunndo commit

--- app/src/main/java/br/com/fiap/placar/PlacarActivity.java
+++ app/src/main/java/br/com/fiap/placar/PlacarActivity.java
@@ -2,12 +2,38 @@ package br.com.fiap.placar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PlacarActivity extends AppCompatActivity {
+
+    private TextView tvTimeCasa;
+    private TextView tvTimeVisitante;
+    private TextView tvPlacarCasa;
+    private TextView tvPlacarVisitante;
+
+    private int golCasa = 0, golVisitante = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placar);
+
+        tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
+        tvTimeVisitante = (TextView) findViewById(R.id.etTimeVisitante);
+        tvPlacarCasa = (TextView) findViewById(R.id.tvPlacarCasa);
+        tvPlacarVisitante = (TextView) findViewById(R.id.etTimeVisitante);
+
+    }
+
+    public void  golCasa(View v) {
+        golCasa ++;
+        tvPlacarCasa.setText(golCasa);
+    }
+
+    public void  golVisitante(View v) {
+        golVisitante ++;
+        tvPlacarVisitante.setText(golVisitante);
     }
 }
